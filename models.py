10dbc7832d764934be510ae6ec1dbd364b1ed93a
Yan Gabriel <yangabrielogy@gmail.com>
2023-03-24 03:24:40 -0300

    fix: imports, documentação e nomes
    importando o explorer para tipagem
    documentação dos modelos mais explicita
    propriedade id -> identity

--- models.py
+++ models.py
@@ -13,12 +13,13 @@ Licença:
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from dataclasses import dataclass, field
+from typing import Any
 
-if TYPE_CHECKING:  # apenas para tipagem estática
-    from league_data.explorer import Explorer
+import league_data
 
 
+@dataclass
 class Champion:
     """Objeto que contém as informações de um campeão.
 
@@ -40,10 +41,30 @@ class Champion:
         ```
     """
 
-    def __init__(self, explorer: Explorer, data: dict) -> None:
-        """Instancia o objeto contendo as informações do campeão."""
-        self.explorer = explorer
-        self.data = data
+    explorer: league_data.Explorer
+    data: dict
+
+    identity: str = field(init=False)
+    name: str = field(init=False)
+    art: str = field(init=False)
+
+    @property
+    def skins(self) -> list[Skin]:
+        """Skins que o campeão possui."""
+        return [
+            Skin(
+                explorer=self.explorer,
+                champion_data=self.data,
+                data=self.data["skins"][skin],
+            )
+            for skin in self.data["skins"]
+        ]
+
+    def __post_init__(self) -> None:
+        """Método utilizado para definir os atributos."""
+        self.identity = self.data["id"]
+        self.name = self.data["name"]
+        self.art = self.data["art"]
 
     def __getitem__(self, value: str) -> Any:
         """Retorna um valor do dicionário do campeão."""
@@ -53,34 +74,8 @@ class Champion:
         """Forma string da classe."""
         return self.name
 
-    @property
-    def identity(self) -> str:
-        """ID do campeão."""
-        return self.data["id"]
 
-    @property
-    def name(self) -> str:
-        """Nome do campeão."""
-        return self.data["name"]
-
-    @property
-    def art(self) -> str:
-        """URL da arte do campeão."""
-        return self.data["art"]
-
-    @property
-    def skins(self) -> list[Skin]:
-        """Skins que o campeão possui."""
-        return [
-            Skin(
-                explorer=self.explorer,
-                champion=self.data,
-                data=self.data["skins"][skin],
-            )
-            for skin in self.data["skins"]
-        ]
-
-
+@dataclass
 class Skin:
     """Objeto que contém as informações de uma skin.
 
@@ -106,11 +101,26 @@ class Skin:
         ```
     """
 
-    def __init__(self, explorer: Explorer, champion: dict, data: dict) -> None:
-        """Instancia o objeto contendo as informações da skin."""
-        self.explorer = explorer
-        self.champion_data = champion
-        self.data = data
+    explorer: league_data.Explorer
+    champion_data: dict
+    data: dict
+
+    identity: str = field(init=False)
+    name: str = field(init=False)
+    art: str = field(init=False)
+    rarity: str = field(init=False)
+
+    @property
+    def champion(self) -> Champion:
+        """O campeão que possui essa skin."""
+        return Champion(self.explorer, self.champion_data)
+
+    def __post_init__(self) -> None:
+        """Método utilizado para definir os atributos."""
+        self.identity = self.data["id"]
+        self.name = self.data["name"]
+        self.art = self.data["art"]
+        self.rarity = self.data["rarity"]
 
     def __getitem__(self, value: str) -> Any:
         """Retorna um valor do dicionário da skin."""
@@ -119,28 +129,3 @@ class Skin:
     def __str__(self) -> str:
         """Forma string da classe."""
         return self.name
-
-    @property
-    def identity(self) -> str:
-        """ID da skin."""
-        return self.data["id"]
-
-    @property
-    def name(self) -> str:
-        """Nome da skin."""
-        return self.data["name"]
-
-    @property
-    def art(self) -> str:
-        """URL da arte da skin."""
-        return self.data["art"]
-
-    @property
-    def rarity(self) -> str:
-        """Raridade da skin."""
-        return self.data["rarity"]
-
-    @property
-    def champion(self) -> Champion:
-        """O campeão que possui essa skin."""
-        return Champion(self.explorer, self.champion_data)
